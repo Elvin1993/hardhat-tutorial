493d45d2d1829e34439279ade00e31e267206314
elvinlong <970099734@qq.com>
2022-11-12 20:34:13 +0800

    init

---
+++ contracts/Token.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
+
+    uint256 public totalSupply = 1000000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    constructor() {
+        console.log('constructor', msg.sender, totalSupply);
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokns");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+      return balances[account];
+    }
+}
---
+++ test/Token.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const { ethers } = require("hardhat");
+
+describe("Token contract", function () {
+  it("Deployment should assign the total supple of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+    console.log('owner', owner.address)
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+
+    const totalSupply = await hardhatToken.totalSupply();
+
+    expect(totalSupply).to.equal(ownerBalance)
+  })
+})
